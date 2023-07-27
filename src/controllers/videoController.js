9e41da02c9cc2f90fdcdd7298d1afd3efb5458f0
Ukja2 <qwe02085@gmail.com>
2023-07-28 01:35:04 +0900

    2023-07-28

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,54 @@
-const fakeUser = {
-  username:"Lee", 
-  loggedIn:false,
-}
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
 
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
 
-
-export const trending = (req, res) => res.render("Home", {pageTitle: "Home", fakeUser:fakeUser});
-export const see = (req, res) => res.render("watch");//파일명은 반드시 소문자로, 파일에 띄어쓰기 금지//
-export const edit = (req,res) => res.render("edit")
-export const search = (req,res) => res.send("Search");
-export const upload = (req,res) => res.send("Upload");
-export const deleteVideo = (req,res) => {
-  return res.send("Delte Video");
+export const postUpload = (req, res) => {
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
-import {see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  getUpload,
+  getEdit,
+  postEdit,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload); 
-videoRouter.get("/:id(\\d+)", see); 
-videoRouter.get("/:id(\\d+)/edit", edit); 
-videoRouter.get("/:id(\\d+)/delete", deleteVideo); 
-//이미 /video 안에 들어왔다는 것을 알기 때문에 알아서 +/watch로 인식 ex)/video/Watch로 적을 필요가 없다는 뜻//
-// " /: " <- 이렇게 표시하는 이유는 express에 이것이 변수라는 것을 알려주기 위함임// 
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
