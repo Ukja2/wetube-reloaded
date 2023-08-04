8151b59c065e30760cd9c5e071eeea2c213e252a
Ukja2 <qwe02085@gmail.com>
2023-08-05 01:28:36 +0900

    2023-08-05

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,10 +54,12 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User")
+export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const logout = (req,res) => res.send("Log out");
+export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
